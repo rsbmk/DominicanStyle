8a008a99d5d9b6379b2b995f9d4e7f4eace63a1d
Roberto Bocio Melo <robertobocio@01lab.co>
2022-05-17 00:07:11 -0500

    [ADD] new types

--- src/types.ts
+++ src/types.ts
@@ -4,5 +4,39 @@ export type Employee = {
   id: number;
   name: string;
   phone: string;
-  roleId: number;
+  teamId: number;
+  team: Team;
+  service: ServiceWithService[];
+};
+
+export type Team = {
+  createAt: string;
+  id: number;
+  name: string;
+};
+
+export type ServiceWithService = {
+  service: {
+    createAt: string;
+    description: string;
+    id: number;
+    name: string;
+    price: number;
+  };
+};
+
+export type Service = {
+  createAt: string;
+  description: string;
+  id: number;
+  name: string;
+  price: number;
+};
+
+export type Appointment = {
+  name: string;
+  telephone: string;
+  shedule: string;
+  serviceId: number;
+  employeeId: number;
 };
