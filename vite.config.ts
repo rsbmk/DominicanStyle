68e4ea4665e9fb8cd6391e018b533c0bed082e2b
Roberto Bocio Melo <robertobocio@01lab.co>
2022-05-17 00:06:51 -0500

    [CONFIG] envarioments

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 // import alias from '@rollup/plugin-alias';
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    // alias({
-    //   entries:{
-    //     'src': './src',
-    //   },
-    // }),
-  ],
-  // test: {
-  //   environment: 'happy-dom'
-  // },
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+
+    plugins: [
+      react(),
+      // alias({
+      //   entries:{
+      //     'src': './src',
+      //   },
+      // }),
+    ],
+    // test: {
+    //   environment: 'happy-dom'
+    // },
+  };
 });
