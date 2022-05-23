29d3a7028d22983ca4b1b29dfd14f40494480982
Roberto Bocio Melo <robertobocio@01lab.co>
2022-05-23 13:59:27 -0500

    confg testing

--- vite.config.ts
+++ vite.config.ts
@@ -7,23 +7,14 @@ export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), "");
-
   return {
     // vite config
+    plugins: [react()],
+    test: {
+      environment: "jsdom",
+    },
     define: {
       __APP_ENV__: env.APP_ENV,
     },
-
-    plugins: [
-      react(),
-      // alias({
-      //   entries:{
-      //     'src': './src',
-      //   },
-      // }),
-    ],
-    // test: {
-    //   environment: 'happy-dom'
-    // },
   };
 });
