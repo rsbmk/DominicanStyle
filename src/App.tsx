f981557d13ff0ccf9de4804998a74f5e91fe09a6
Roberto Bocio Melo <robertobocio@01lab.co>
2022-08-21 17:26:01 -0500

    [ADD] new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { Route } from "wouter";
+import { Route, Switch } from "wouter";
 
 const Appointment = lazy(() => import("@/pages/appointment"));
 const Home = lazy(() => import("@/pages/home"));
@@ -7,8 +7,25 @@ const Home = lazy(() => import("@/pages/home"));
 function App() {
   return (
     <Suspense fallback={null}>
-      <Route path="/" component={Home} />
-      <Route path="/appointment" component={Appointment} />
+      <Switch>
+        <Route path="/" component={Home} />
+
+        {/* appointment */}
+        <Route path="/appointment" component={Appointment} />
+
+        {/* Client */}
+        <Route path="/client/register">
+          <p>no he creado esta pagina</p>
+        </Route>
+
+        {/* Employee */}
+        <Route path="/employee/login">
+          <p>no he creado esta pagina</p>
+        </Route>
+        <Route>
+          <span>404, Not Found!</span>
+        </Route>
+      </Switch>
     </Suspense>
   );
 }
