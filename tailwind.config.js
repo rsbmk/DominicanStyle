efef2789ba818247746df2968d1900f37ee05e6b
Roberto Bocio Melo <robertobocio@01lab.co>
2022-08-21 12:50:00 -0500

    [ADD] primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          100: "#0099ff",
+        },
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
