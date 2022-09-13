5c939c925615db529b8846d38f1c2ef8c6e29cfd
Roberto Bocio Melo <robertobocio@01lab.co>
2022-09-12 19:31:04 -0500

    [ADD] contants for reset errors and appointments type

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+export const resetErrors = {
+  input: "",
+  message: "",
+};
+
+export const appointmentState = {
+  pending: "Pendiente",
+  confirmed: "Confirmado",
+  process: "En proceso",
+  close: "Cerrado",
+  cancelled: "Cancelado"
+}
