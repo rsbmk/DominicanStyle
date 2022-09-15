41929c6feadbee77ddfbc4fa94702b5815796fa4
Roberto Bocio Melo <robertobocio@01lab.co>
2022-09-14 21:35:18 -0500

    [ADD] axios

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,18 +1,20 @@
+import axios from "axios";
 import { Client } from "@/types";
 
-const BASE_URI = import.meta.env.VITE_BASE_API_URL;
+const BASE_URI = import.meta.env.VITE_BASE_API_URL ?? "http://localhost:8080/api";
 
 export const searchClientData = async ({ clientId }: { clientId: string }) => {
   try {
-    const response = await fetch(`${BASE_URI}/v1/client/${clientId}`);
+    const { data, status } = await axios.get(`${BASE_URI}/v1/client/${clientId}`);
 
-    if (response.ok) {
-      return (await response.json()) as Client;
-    }
+    if (status === 200) return data as Client;
 
-    throw await response.json();
+    throw new Error("Error fetching client data");
   } catch (error) {
-    console.error("useClient ~ searchClientData ~ line 13", error);
+    if (axios.isAxiosError(error)) {
+      throw error.response;
+    }
+    console.error("useClient ~ searchClientData ~ line 13", { error });
     throw error;
   }
 };
