a506ace0b26bcc902a803fc8e41e1a6d7788e12f
Roberto Bocio Melo <robertobocio@01lab.co>
2022-09-14 21:43:02 -0500

    [ADD] msw

---
+++ src/mocks/data.ts
@@ -0,0 +1,7 @@
+export const clientDataMock = {
+  cedula: "1757646805",
+  name: "Kerobe",
+  last_name: "Melo",
+  telephone: "093569846",
+  createAt: "2022-08-20T17:58:06.000Z",
+};
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,54 +1,15 @@
 import { rest, RestRequest, ResponseComposition, RestContext } from "msw";
-import { Appointment, Employee } from "../types";
-const BASE_URL = import.meta.env.VITE_BASE_API_URL;
-
-const EmployeeData: Employee[] = [
-  {
-    id: 1,
-    name: "John Doe",
-    address: "123 Main St",
-    createdAt: new Date().toISOString(),
-    phone: "123456789",
-    service: [
-      {
-        service: {
-          id: 1,
-          name: "Service 1",
-          price: 100,
-          createAt: new Date().toISOString(),
-          description: "Service 1 description",
-        },
-      },
-    ],
-    team: {
-      id: 1,
-      createAt: new Date().toISOString(),
-      name: "Team 1",
-    },
-    teamId: 1,
-  },
-];
-
-const AppointmenData: Appointment[] = [
-  {
-    employeeId: 1,
-    serviceId: 1,
-    name: "John Doe",
-    telephone: "123456789",
-    schedule: "2020-01-01T00:00:00.000Z",
-  },
-];
+import { clientDataMock } from "./data";
+const BASE_URL = 'http://localhost:8080/api/v1'
 
 export const handlers = [
-  rest.get(`${BASE_URL}/v1/employees`, handleGetEmployees),
-  rest.post(`${BASE_URL}/v1/appointment`, handleCreateAppointment),
+  rest.get(`${BASE_URL}/client/:cedula`, handleGetClient),
+
 ];
 
-function handleCreateAppointment(req: RestRequest, res: ResponseComposition, ctx: RestContext) {
-  console.log("handleCreateAppointment", req.body);
-  return res(ctx.delay(0), ctx.status(201), ctx.json(req.body));
-}
+function handleGetClient(req: RestRequest, res: ResponseComposition, ctx: RestContext) {
+  const { cedula } = req.params;
 
-function handleGetEmployees(req: RestRequest, res: ResponseComposition, ctx: RestContext) {
-  return res(ctx.delay(0), ctx.status(200), ctx.json(EmployeeData));
+  if (cedula === clientDataMock.cedula) return res(ctx.delay(0), ctx.status(200), ctx.json(clientDataMock));
+  return res(ctx.delay(0), ctx.status(404), ctx.json({ "nameInput": 'cedula', 'message': 'Error al buscar el cliente' }));
 }
